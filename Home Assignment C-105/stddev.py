297fa7bdbb0c6cfe1054d8ac747bb43a4199ab9e
Prajit-Shandilya <66900497+Prajit-Shandilya@users.noreply.github.com>
2022-02-06 13:46:25 +0530

    Add files via upload

---
+++ Home Assignment C-105/stddev.py
@@ -0,0 +1,33 @@
+import math
+import csv
+with open('data.csv', newline="")as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+data = file_data[0]
+
+def mean(data):
+    n =len(data)
+    total = 0
+    for x in data:
+        total =+ int(x)
+    mean = total/ n
+    return mean
+
+square_list=[]
+
+for number in data:
+    a= int(number) - mean(data)
+    a=a**2
+    square_list.append(a)
+
+sum = 0
+for i in square_list:
+    sum=sum+i
+
+
+results = sum/(len(data)-1)
+print(data)
+print(square_list)
+
+std_dev = math.sqrt(results)
+print(f"Standard Deviation is-> {std_dev}")
